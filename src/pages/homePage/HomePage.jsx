d65438772a2a3ca0a423319843676fb0149b251e
Vitalii-250885 <provitaleks@gmail.com>
2024-04-22 17:01:17 +0300

    edit home page

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -8,6 +8,22 @@ const HomePage = () => {
         <img src="/logo.png" alt="logo" className={css.logo} />
         <div className={css["title-options"]}>
           <h1 className={css.title}>{"Слов'янський Баптистський Інститут"}</h1>
+
+          <div className={css["verses-container"]}>
+            <p className={css.verse}>
+              Отже, мій сину, міцній у благодаті, яка в Ісусі Христі. І що ти
+              почув від мене при багатьох свідках, те передай вірним людям, які
+              будуть спроможні й інших навчити.
+            </p>
+            <p className={css.reference}>2 Тимофія 2:1-2</p>
+          </div>
+          <div className={css["verses-container"]}>
+            <p className={css.verse}>
+              Постарайся представити себе гідним перед Богом, бездоганним
+              працівником, який наставляє на принципи істини.
+            </p>
+            <p className={css.reference}>2 Тимофія 2:15</p>
+          </div>
           <nav className={css.options}>
             <NavLink to="/news" className={css.button}>
               Новини
