59ceeafd1c548f72c1a4f2121abe9c059dfee5aa
Vitalii-250885 <provitaleks@gmail.com>
2024-04-22 16:01:12 +0300

    add contacts

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,22 +3,18 @@ import css from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={css.footer}>
-      {/* <ul className={css["contacts-list"]}>
+      <ul className={css["contacts-list"]}>
         <li className={css["contacts-item"]}>
-          <img
-            src="/png-transparent-facebook-logo-computer-icons-facebook-blue-trademark-logo.png"
-            alt="facebook"
-            className={css.icon}
-          />
+          <a href="https://www.facebook.com/sbiukraine?locale=uk_UA">
+            <img src="facebook.png" alt="facebook" className={css.icon} />
+          </a>
         </li>
         <li className={css["contacts-item"]}>
-          <img
-            src="/pngtree-three-dimensional-instagram-icon-png-image_6618437.png"
-            alt="Instagram"
-            className={css.icon}
-          />
+          <a href="https://www.instagram.com/sbiukr/">
+            <img src="instagram.png" alt="Instagram" className={css.icon} />
+          </a>
         </li>
-        <li className={css["contacts-item"]}>
+        {/* <li className={css["contacts-item"]}>
           <img
             src="/png-clipart-telephone-call-computer-icons-symbol-telephone-icon-miscellaneous-logo-thumbnail.png"
             alt="Phone"
@@ -27,8 +23,8 @@ const Footer = () => {
         </li>
         <li className={css["contacts-item"]}>
           <img src="/email.png" alt="mail" className={css.icon} />
-        </li>
-      </ul> */}
+        </li> */}
+      </ul>
     </div>
   );
 };
