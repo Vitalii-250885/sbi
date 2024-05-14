33d1ff96deb66258ab3dc4b06a2d7ab69b43527a
Vitalii-250885 <provitaleks@gmail.com>
2024-05-14 22:14:34 +0300

    add applicants

--- src/pages/applicantsPage/ApplicantsPage.jsx
+++ src/pages/applicantsPage/ApplicantsPage.jsx
@@ -1,5 +1,9 @@
+import Appearance from "../../components/appearance/Appearance";
+import Finance from "../../components/finance/Finance";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
+import Requirements from "../../components/requirements/Requirements";
+import Rules from "../../components/rules/Rules";
 
 import css from "./ApplicantsPage.module.css";
 
@@ -9,6 +13,10 @@ const applicantsPage = () => {
       <Header />
       <div className={css.applicants}>
         <h2 className={css.title}>Абітурієнтам</h2>
+        <Requirements />
+        <Finance />
+        <Appearance />
+        <Rules />
       </div>
       <Footer />
     </>
