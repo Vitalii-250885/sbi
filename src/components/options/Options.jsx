c3907883354e1d96264c6b84b1cff0a9169c313e
Vitalii-250885 <provitaleks@gmail.com>
2024-09-23 15:23:56 +0300

    correct

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -14,24 +14,24 @@ const Options = ({ closeMenu, isAlwaysVisible }) => {
 
   return (
     <nav className={css[`${buildVisibleOptions()}`]}>
-      <NavLink to="/events" className={buildLinkClass} onClick={closeMenu}>
+      {/* <NavLink to="/events" className={buildLinkClass} onClick={closeMenu}>
         Події
-      </NavLink>
+      </NavLink> */}
       <NavLink to="/about" className={buildLinkClass} onClick={closeMenu}>
         Про нас
       </NavLink>
-      {/* <NavLink to="/applicants" className={buildLinkClass} onClick={closeMenu}>
-            Абітурієнтам
-          </NavLink> */}
+      <NavLink to="/applicants" className={buildLinkClass} onClick={closeMenu}>
+        Абітурієнтам
+      </NavLink>
       <NavLink to="/contacts" className={buildLinkClass} onClick={closeMenu}>
         Контакти
       </NavLink>
       {/* <NavLink to="/register" className={buildLinkClass} onClick={closeMenu}>
         Реєстрація
       </NavLink> */}
-      {/* <NavLink to="/user" className={buildLinkClass} onClick={closeMenu}>
+      <NavLink to="/user" className={buildLinkClass} onClick={closeMenu}>
         Увійти
-      </NavLink> */}
+      </NavLink>
     </nav>
   );
 };
